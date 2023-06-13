33c6717a362c72a266aa51a7122ca7bf267e3255
Ray_Huang <raymineg@gmail.com>
2023-06-13 10:52:07 +0800

    update

--- Opencv_Video_Upscaling.py
+++ Opencv_Video_Upscaling.py
@@ -2,9 +2,9 @@ import cv2
 import moviepy.editor as mp
 
 # 加载视频文件 cv2.resize()
-fileName = "VID_20230510_203409_00_005"
+fileName = "HarryPotter"
 #video = cv2.VideoCapture(f"C:/Python/Video_Python/{fileName}.mp4")
-video = cv2.VideoCapture(f"D:/Python/{fileName}.mp4")
+video = cv2.VideoCapture(f"D:/Python/研究所/360_degreePanoramaVideoUpscaling/{fileName}.mp4")
 
 # 检查视频是否成功打开
 if not video.isOpened():
@@ -19,12 +19,12 @@ total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 
 # 定義8K影像畫質
-new_width = 7680
-new_height = 4320
+new_width = 5760 #7680
+new_height = 2880 #4320
 
 # 创建一个VideoWriter对象，用于保存处理后的视频
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 转换为MP4格式
-out = cv2.VideoWriter(f"D:/Python/output.mp4", fourcc, fps, (new_width, new_height))
+out = cv2.VideoWriter(f"D:/Python/研究所/360_degreePanoramaVideoUpscaling/output.mp4", fourcc, fps, (new_width, new_height))
 
 
 # 设置调整参数
@@ -39,11 +39,11 @@ while video.isOpened():
     # 如果读取到了视频帧，就将其放大并写入输出视频
     if ret:
         #雙立方內插法(Bicubic interpolation)放大尺寸
-        resized_frame = cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_CUBIC)  
-
+        blurred = cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_CUBIC)  
+        #resized_frame
 
         # 使用高斯濾波器減少噪點
-        blurred = cv2.GaussianBlur(resized_frame, (5, 5), 0)
+        #blurred = cv2.GaussianBlur(resized_frame, (5, 5), 0)
         #out.write(blurred)
      
         # 使用高斯后的图像，计算其目标位置# 进行去除交错
@@ -82,18 +82,18 @@ out.release()
 
 #===================加載聲音=======================
 # 加载原始视频文件
-video_with_audio = mp.VideoFileClip(f"D:/Python/{fileName}.mp4")
+video_with_audio = mp.VideoFileClip(f"D:/Python/研究所/360_degreePanoramaVideoUpscaling/{fileName}.mp4")
 
-video = cv2.VideoCapture(f"D:/Python/{fileName}.mp4")
+video = cv2.VideoCapture(f"D:/Python/研究所/360_degreePanoramaVideoUpscaling/{fileName}.mp4")
 
 # 读取原始视频中的音频
 audio = video_with_audio.audio
 
 # 创建一个新的视频文件，将处理后的视频和原始音频合并
-final_video = mp.VideoFileClip(f"D:/Python/output.mp4").set_audio(audio)
+final_video = mp.VideoFileClip(f"D:/Python/研究所/360_degreePanoramaVideoUpscaling/output.mp4").set_audio(audio)
 
 # 保存最终的视频文件
-final_video.write_videofile(f"D:/Python/{fileName}_final_output.mp4")
+final_video.write_videofile(f"D:/Python/研究所/360_degreePanoramaVideoUpscaling/{fileName}_final_output.mp4")
 
 # 清理资源
 final_video.close()
