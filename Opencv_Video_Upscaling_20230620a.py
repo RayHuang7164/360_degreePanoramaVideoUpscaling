ea051e0fd9ceb75b01e9122b6633ad232763c19f
RayHuang7164 <raymineg@gmail.com>
2023-06-26 11:36:03 +0800

    update

--- Opencv_Video_Upscaling_20230620a.py
+++ Opencv_Video_Upscaling_20230620a.py
@@ -39,7 +39,7 @@ total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 # 創建一個VideoWriter對象，用於保存處理後的視頻
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 轉換為MP4格式
 out = cv2.VideoWriter(output_file, fourcc, new_fps, (new_width, new_height))
-
+#~~~~
 # 循環遍歷視頻的每一幀
 while video.isOpened():
     ret, frame = video.read()
